71d116103db5ed202cb1efa86df0f0b7508dc495
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2012-11-14 11:18:22 +0900

    First stat!

--- 4sqstats.rb
+++ 4sqstats.rb
@@ -3,4 +3,21 @@ require "JSON"
 
 client = Foursquare2::Client.new(:oauth_token => 'ADB02WREAK4W4R5BDYBVEXHWB14VZM4TQOIWZCYAD1GY22EK')
 
-puts client.user_venue_history()
+checkins_a = {}
+
+client.user_venue_history().items.each do |v|
+	if v.venue.categories[0]
+		if v.venue.categories[0].parents
+			#puts "#{v.beenHere} #{v.venue.name} #{v.venue.categories[0].parents[0]}"
+			checkins_a["#{v.venue.name} - #{v.venue.categories[0].parents[0]}"] = v.beenHere;
+		else
+			#puts "#{v.beenHere} #{v.venue.name} #{v.venue.categories[0]}"
+			checkins_a["#{v.venue.name} - #{v.venue.categories[0]}"] = v.beenHere;
+		end
+	else
+		#puts "#{v.beenHere} #{v.venue.name}"
+		checkins_a["#{v.venue.name}"] = v.beenHere;
+	end
+end
+
+puts checkins_a.sort_by {|k,v| v}.reverse
