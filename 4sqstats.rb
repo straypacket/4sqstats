b3f193e5811391f4ff6597e59d1f2c4f4d80b653
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2012-11-16 11:54:26 +0900

    Enhached for debugging/feature study

--- 4sqstats.rb
+++ 4sqstats.rb
@@ -4,8 +4,9 @@ require "JSON"
 client = Foursquare2::Client.new(:oauth_token => 'ADB02WREAK4W4R5BDYBVEXHWB14VZM4TQOIWZCYAD1GY22EK')
 
 checkins_a = {}
+history = client.user_venue_history().items
 
-client.user_venue_history().items.each do |v|
+history.each do |v|
 	if v.venue.categories[0]
 		if v.venue.categories[0].parents
 			#puts "#{v.beenHere} #{v.venue.name} #{v.venue.categories[0].parents[0]}"
@@ -21,3 +22,6 @@ client.user_venue_history().items.each do |v|
 end
 
 puts checkins_a.sort_by {|k,v| v}.reverse
+
+#puts history[0]
+#puts history[-1]
