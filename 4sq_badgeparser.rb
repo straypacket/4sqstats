d974cc8b627fddaa9728d73ed0e7b58edbf752ee
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-15 09:54:22 +0900

    Parse sub pages

--- 4sq_badgeparser.rb
+++ 4sq_badgeparser.rb
@@ -5,15 +5,24 @@ require 'mongo'
 site = "http://mattersofgrey.com/foursquare-badge-list/"
 
 doc = Nokogiri::HTML(open(site))
-doc.remove_namespaces!
 
 # List all nodes path
 #doc.traverse {|node| puts node.path}
 
 # New badges
 doc.search('//table[1]/tr/td[1]/a[normalize-space()]').each do |b|
-	puts b.text
-	puts b.values
+	badge = b.text.gsub(/^\s+/, "")
+	subsite = b.values[0].to_s
+
+	puts ("#{badge} - #{subsite}")
+
+	# Parse badge site
+	subdoc = Nokogiri::HTML(open(subsite))
+
+	# Get stats
+	subdoc.search('//html/body/div/div[2]/div[1]/ul/li[1]/div/div[2][normalize-space()]').each do |bs|
+		puts bs.text.gsub(/^\s+/, "").scan(/"([^"]*)"/)
+	end
 end
 
 # Core badges
